8e8f15c5d40b6f8a3d5519742d2b0631f144a634
CodeYouness <ylijassi@gmail.com>
2024-05-09 18:15:23 +0200

    Milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const priceForKm = '0.267113';
 const discountYoung = '24.552';
 const discountOld = '37.893';
-let userKM = document.querySelector('#distance')
-let userAge = document.querySelector('#age')
 const bottone = document.querySelector('button')
-let comparisonA = userAge < 21;
-let comparisonB = userAge > 63;
-let price = userKM * priceForKm;
+
+// let price = userKM * priceForKm;
 
 
 bottone.addEventListener('click', function () {
-    let
+    const userKM = document.querySelector('#distance').value
+    const userAge = document.querySelector('#age').value
+    let comparisonA = userAge < 21;
+    let comparisonB = userAge > 63;
+
+
+    if (comparisonA === true) {
+        let price = ((userKM * priceForKm) - ((userKM * priceForKm) / 100) * discountYoung)
+        console.log(price.toFixed(2) + '€');
+    } else if (comparisonB == true) {
+        let price = ((userKM * priceForKm) - ((userKM * priceForKm) / 100) * discountOld)
+        console.log(price.toFixed(2) + '€');
+    } else {
+        let price = (userKM * priceForKm)
+        console.log(price.toFixed(2) + '€');
+    }
 })
 
