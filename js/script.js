b940383baf48745589087e4dfbff152fa4cc6bfd
CodeYouness <ylijassi@gmail.com>
2024-05-09 17:19:22 +0200

    added input and button on html added let and const with price, discount and userkm/age

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const priceForKm = '0.267113';
+const discountYoung = '24.552';
+const discountOld = '37.893';
+let userKM = document.querySelector('#distance')
+let userAge = document.querySelector('#age')
+const bottone = document.querySelector('button')
+let comparisonA = userAge < 21;
+let comparisonB = userAge > 63;
+let price = userKM * priceForKm;
+
+
+bottone.addEventListener('click', function () {
+    let
+})
+
